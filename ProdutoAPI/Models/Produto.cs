f55b4947b95a46c4c1adeb6f88eb582311a6c91d
Luigi Matheus Afornalli Breda <luigi.breda_t@meuk8.com.br>
2024-12-18 20:59:56 -0300

    Commit inicial

---
+++ ProdutoAPI/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProdutoAPI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+    }
+}
