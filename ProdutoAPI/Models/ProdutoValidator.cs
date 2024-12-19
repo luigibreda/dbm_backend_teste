f3b0cf637c8c60e04dd07595aa35549ee80c47df
Luigi Matheus Afornalli Breda <luigi.breda_t@meuk8.com.br>
2024-12-18 22:09:00 -0300

    Implementando o FluentValidation e algumas validações no Model de Produto

---
+++ ProdutoAPI/Models/ProdutoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using ProdutoAPI.Models;
+
+public class ProdutoValidator : AbstractValidator<Produto>
+{
+    public ProdutoValidator()
+    {
+        RuleFor(p => p.Nome)
+            .NotEmpty().WithMessage("O nome é obrigatório.")
+            .MaximumLength(100).WithMessage("O nome não pode exceder 100 caracteres.");
+
+        RuleFor(p => p.Preco)
+            .GreaterThan(0).WithMessage("O preço deve ser maior que zero.");
+
+        RuleFor(p => p.Descricao)
+            .MaximumLength(250).WithMessage("A descrição não pode exceder 250 caracteres.");
+    }
+}
--- ProdutoAPI/Program.cs
+++ ProdutoAPI/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using ProdutoAPI.Data;
 
@@ -5,7 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddFluentValidation(config =>
+    {
+        config.RegisterValidatorsFromAssemblyContaining<Program>();
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
