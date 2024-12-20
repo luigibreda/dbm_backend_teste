62e0ccd2d173fa9f69cced88dcc624d53493ff58
Luigi Matheus Afornalli Breda <luigi.breda_t@meuk8.com.br>
2024-12-20 20:09:05 -0300

    Home modificada com detalhes sobre o teste e sobre mim.

--- produto-front-nextjs/app/page.tsx
+++ produto-front-nextjs/app/page.tsx
@@ -1,100 +1,94 @@
 import Image from "next/image";
+import { AvatarIcon, Button } from "@nextui-org/react"; // Importando o componente Button do NextUI
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-gradient-to-r from-teal-700 to-blue-600 p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col justify-between">
+      <header className="flex justify-between items-center mb-12">
+        <nav className="flex gap-6">
+          <a
+            className="text-white hover:underline"
+            href="https://github.com/luigibreda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            className="text-white hover:underline"
+            href="https://linkedin.com/in/luigibreda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </nav>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <main className="flex flex-col items-center text-center text-white">
+        <h1 className="text-5xl sm:text-6xl font-semibold mb-4 bg-gradient-to-r from-teal-300 via-yellow-300 to-pink-500 bg-clip-text text-transparent">
+          Front-End Next.js + Back-End .NET 8
+        </h1>
+        <p className="text-lg sm:text-xl mb-8 max-w-3xl mx-auto">
+          Conectando Front-End com Next.js e APIs robustas em .NET 8.
+        </p>
+
+        <div className="mb-8 flex justify-center gap-6">
+          <Button
+            as="a"
+            href="produto"
+            color="primary"
+            size="lg"
+            variant="solid"
+            className="w-44 transition-all duration-300"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            VER PRODUTOS
+          </Button>
+        </div>
+
+        <div className="max-w-4xl mx-auto text-left">
+          <h2 className="text-3xl text-yellow-300 font-semibold mb-4">Sobre mim</h2>
+          <p className="text-lg mb-4">
+            Sou <strong>Luigi Breda</strong>, desenvolvedor full-stack com foco em soluções escaláveis e de alta performance. Tenho experiência com tecnologias como <strong>Next.js</strong>, <strong>.NET 8</strong>, <strong>SQL Server</strong> e outras.
+          </p>
+          <p className="text-lg mb-4">
+            Busco uma oportunidade na <strong>Netrin</strong>, onde posso aplicar minhas habilidades para criar soluções robustas e de alta qualidade.
+          </p>
+
+          <h2 className="text-3xl text-yellow-300 font-semibold mb-4">Tecnologias Utilizadas</h2>
+          <ul className="list-disc pl-5 text-lg">
+            <li><strong>Next.js</strong> (React) para a criação do Front-End interativo e escalável.</li>
+            <li><strong>.NET 8</strong> para o desenvolvimento de Back-End com APIs seguras e eficientes.</li>
+            <li><strong>SQL Server</strong> como sistema de gerenciamento de banco de dados.</li>
+            <li><strong>FluentMigrator</strong>: Utilizado para a gestão de migrações de banco de dados e criação da tabela <strong>Produtos</strong>.</li>
+            <li><strong>FluentValidation</strong>: Implementação de validações para garantir que dados como Nome (máx. 100 caracteres) e Preço (não nulo e maior que zero) sejam validados corretamente.</li>
+            <li><strong>xUnit</strong>: Utilização de testes unitários para garantir a qualidade do código e cobrir operações do CRUD (Repository e Service), além das validações do FluentValidation.</li>
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="bg-gradient-to-r from-teal-600 to-blue-700 text-white py-8 mt-16">
+        <div className="text-center">
+          <p className="text-sm">&copy; 2024 Luigi Breda - Todos os direitos reservados.</p>
+          <div className="flex justify-center gap-4 mt-4">
+            <a
+              className="text-white hover:text-gray-400"
+              href="https://github.com/luigibreda"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              className="text-white hover:text-gray-400"
+              href="https://linkedin.com/in/luigibreda"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
       </footer>
     </div>
   );
